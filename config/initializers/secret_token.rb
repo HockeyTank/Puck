baf322b399ecad684b177f5362a0daa67a225543
Charles Butler <chuck@dasroot.net>
2012-10-27 00:44:56 -0400

    DataEntry app Puck

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puck::Application.config.secret_token = '28fd40e1dc48c638a02c03ab428eb2c80e4e3a0f5779f49cb741cb514d4d130076f1b4ee4357b526b46ac4d8c4698e6225bb1062549f0da5b14c8a9dbf84d928'
