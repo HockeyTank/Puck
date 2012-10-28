b94249dec14259b29a5a0f578c1c0baad7c47067
Charles Butler <chuck@dasroot.net>
2012-10-28 18:01:52 -0400

    Added nested model support for rink surfaces

--- app/models/rink.rb
+++ app/models/rink.rb
@@ -1,17 +1,9 @@
 class Rink < ActiveRecord::Base
   attr_accessible :address1, :address2, :addresscity, :addressstate, :addresszip, :indoor, :name, :notes, :openhockey, :phone1, :phone2, :proshop, :sticktime, :surfacecount, :website,
-  :surfaces_attributes 
+  :rink_surfaces_attributes 
 
-  #has_many :league_rinks 
-  #has_many :rink_leagues, :through => :league_rinks
-  
-  #has_many :rink_developmental_programs
-  #has_many :developmental_programs, :through => :rink_developmental_programs
- 
   has_many :rink_surfaces
   has_many :surfaces, :through => :rink_surfaces  
   
-  #accepts_nested_attributes_for :rink_leagues
-  accepts_nested_attributes_for :surfaces, :reject_if => :all_blank, :allow_destroy => true
-  #accepts_nested_attributes_for :developmental_programs
+  accepts_nested_attributes_for :rink_surfaces, :reject_if => :all_blank, :allow_destroy => true
 end
--- app/models/surface.rb
+++ app/models/surface.rb
@@ -1,5 +1,6 @@
 class Surface < ActiveRecord::Base
   attr_accessible :name
+  
   has_many :rink_surfaces
   has_many :rinks, :through => :rink_surfaces
 end
