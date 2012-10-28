cec2c63a1b90365f772bae6b1f5bf8cec5e6e42c
Charles Butler <chuck@dasroot.net>
2012-10-28 19:06:22 -0400

    Added support for dynamic rink developmental programs

--- app/models/rink.rb
+++ app/models/rink.rb
@@ -1,9 +1,13 @@
 class Rink < ActiveRecord::Base
   attr_accessible :address1, :address2, :addresscity, :addressstate, :addresszip, :indoor, :name, :notes, :openhockey, :phone1, :phone2, :proshop, :sticktime, :surfacecount, :website,
-  :rink_surfaces_attributes 
+  :rink_surfaces_attributes, :rink_developmental_programs_attributes
 
   has_many :rink_surfaces
   has_many :surfaces, :through => :rink_surfaces  
-  
+  has_many :rink_developmental_programs
+  has_many :developmental_programs, :through => :rink_developmental_programs 
+
+ 
   accepts_nested_attributes_for :rink_surfaces, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :rink_developmental_programs, :reject_if => :all_blank, :allow_destroy => true
 end
